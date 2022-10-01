01880371a7497362aa689bd3124a33ebf144d6de
chuheeseung <youverygood@naver.com>
2022-10-01 14:00:49 +0900

    22.10.01 - 서버 배포 완료

--- card-maker/src/App.js
+++ card-maker/src/App.js
@@ -3,7 +3,6 @@ import { Route, Routes, BrowserRouter } from 'react-router-dom';
 import './index.css';
 import StartScreen from './routes/StartScreen';
 import MainScreen from './routes/MainScreen';
-import NotFound from './routes/NotFound';
 
 const App = () => {
 	return (
@@ -11,7 +10,6 @@ const App = () => {
 			<Routes>
 				<Route path="/" element={<StartScreen />} />
 				<Route path="/main" element={<MainScreen />} />
-				<Route path="/*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
 	);
